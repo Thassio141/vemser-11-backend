0505e695c99a82a3c74e4d11bb686b22ef616045
Thassio <thassio.vagner1@gmail.com>
2023-02-07 14:38:57 -0300

    correcao questao 2 lista 2

--- modulo1/lista2/Questao2.java
+++ modulo1/lista2/Questao2.java
@@ -2,25 +2,24 @@ import java.util.Scanner;
 public class Questao2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int numero , chute;
+        int numero, chute;
 
         System.out.println("Forneça um numero para que outra pessoa tente adivinhar: ");
         numero = sc.nextInt();
 
-        System.out.println("Tente adivinhar o numero: ");
-        chute = sc.nextInt();
+        while (true) {
+            System.out.println("Tente adivinhar o numero: ");
+            chute = sc.nextInt();
 
-        if ( chute == numero) {
-            System.out.println("Você Acertou!");
-        }
-        else{
-            while (chute != numero){
-                System.out.println("Tente adivinhar o numero: ");
-                chute = sc.nextInt();
-                if ( chute == numero) {
-                    System.out.println("Você Acertou!");
-                }
+            if (chute == numero) {
+                System.out.println("Você Acertou!");
+                break;
+            } else if (chute > numero) {
+                System.out.println("O numero é menor que " + chute);
+            } else {
+                System.out.println("O numero é maior que " + chute);
             }
+
         }
     }
 }
