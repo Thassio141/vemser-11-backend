037bca8295f2ebaeed662dc8b26ecfff4eeac724
Thassio <thassio.vagner1@gmail.com>
2023-02-10 13:51:13 -0300

    correcao conta corrente 3

--- modulo1/conta-corrente3/ContaPagamento.java
+++ modulo1/conta-corrente3/ContaPagamento.java
@@ -5,7 +5,6 @@ public class ContaPagamento extends Conta implements Impressao{
     public ContaPagamento(Cliente cliente, String numeroConta, String agencia, double saldo) {
         super(cliente, numeroConta, agencia, saldo);
     }
-
     @Override
     public boolean sacar(double valor){
         if ( valor <= 0 ){
@@ -20,6 +19,22 @@ public class ContaPagamento extends Conta implements Impressao{
             return false;
         }
     }
+
+    @Override
+    public boolean transferir(Conta conta, double valor){
+        if ( valor <= 0 ){
+            return false;
+        }
+        else if  (getSaldo() >= valor + TAXA_SAQUE){
+            conta.depositar(valor);
+            System.out.println("tranferencia de R$" + valor + " realizado com sucesso!");
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
     @Override
     public void imprimir() {
         System.out.printf("Conta Pagamento > Numero da Conta: %s Agencia: %s Saldo: %f\n",getNumeroConta(),getAgencia(),getSaldo());
