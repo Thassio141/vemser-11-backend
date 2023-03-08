8b12df5f14bf760314579e7a074e62ccdb7c1e17
Thassio <thassio.vagner1@gmail.com>
2023-03-08 14:10:44 -0300

    adicionando documentacao a id DTO

--- modulo-3-1/pessoa-api/src/main/java/br/com/dbc/vemser/pessoaapi/dto/EnderecoDTO.java
+++ modulo-3-1/pessoa-api/src/main/java/br/com/dbc/vemser/pessoaapi/dto/EnderecoDTO.java
@@ -1,8 +1,10 @@
 package br.com.dbc.vemser.pessoaapi.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class EnderecoDTO extends EnderecoCreateDTO{
+    @Schema(description = "Id do Endereco" , example = "1" , required = true)
     private Integer idEndereco;
 }
